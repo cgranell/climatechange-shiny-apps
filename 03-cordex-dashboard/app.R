34fe5054a8a06ac07c77c4d5c51286fc6f42bb18
cgranell <carlos.granell.canut@gmail.com>
2018-02-02 11:46:41 +0100

    Update

--- 03-cordex-dashboard/app.R
+++ 03-cordex-dashboard/app.R
@@ -1,50 +1,108 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is a Shiny dashboard web application.
+# 
+# https://shiny.rstudio.com/articles/dashboards.html
+# http://rstudio.github.io/shinydashboard/get_started.html
+
 
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(highcharter)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+source("global.R")
+
+header <- dashboardHeader(
+  title = "Climate simulation in major European cities (Marek)",
+  titleWidth = 450)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem("dashboard",
+            fluidRow(
+              valueBoxOutput("modelInfo"),
+              valueBoxOutput("indexInfo"),
+              valueBoxOutput("appInfo")
+            ),
+            fluidRow(
+              column(width = 8,
+                box(
+                  width = NULL, status = "info", solidHeader = TRUE,
+                  title = "Decade chart", 
+                  highchartOutput("hcontainer", width="100%", height = "500px"))
+              ),
+              column(width = 4,
+                box(width = NULL, status = "warning",
+                    uiOutput("indexSelect")
+                )
+              )
+            )
+          ),
+    tabItem("explorer"),
+    tabItem("about", 
+            fluidRow(
+              box(title = "Help", status = "warning",
+                  p(
+                    class = "text-muted",
+                    paste("Use the Dashboard tab to compare diffent simulation plots. ",
+                          "Use the Explorer tab to examine the raw data in tabular form. ",
+                          "To learn more about the project, visit GEO-C project: http://www.geo-c.eu. ",
+                          "Application author: Marek Smid.")
+                    )
+                )
+            ))
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard"),
+    menuItem("Data explorer", tabName = "explorer"),
+    menuItem("About", tabName = "about"),
+    box("Use the Dashboard tab to compare diffent simulation plots. ", br(),
+        "Use the Explorer tab to examine the raw data in tabular form. ", br(),
+        "To learn more about the project, visit GEO-C project: http://www.geo-c.eu. ", br(),
+        "Application author: Marek Smid.")
+    )
+)
+  
+
+ui <- dashboardPage(skin = "blue",
+  header,
+  sidebar,
+  body
+)
+
+
+
+
+
+
+server <- function(input, output) { 
+  
+  output$modelInfo <- renderValueBox({
+    
+    current_model <- "CORDEX"
+    valueBox(
+      value = current_model,
+      subtitle = "Selected model",
+      icon = icon("download")
+    )
+  })
+  
+  
+  output$indexInfo <- renderInfoBox({
+    
+    current_index <- input$index
+    valueBox(
+      value = current_index,
+      subtitle = "Selected index",
+      icon = icon("list")
+    )
+  })
+  
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
---
+++ 03-cordex-dashboard/global.R
@@ -0,0 +1,3 @@
+# Define the list of options
+indices <- structure(c("hwmid", "tn10p"), 
+                     .Names = c("Heat Wave Magnitude Index-daily (HWMId)", "Cold nights (TN10P)"))
